3216481f50216bb4d5bec2dae783876389b26c42
Pakchan <1625787@donga.ac.kr>
2024-02-08 10:37:51 +0900

    글목록보기 구현 완료

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,16 +1,27 @@
 package shop.mtcoding.blog.board;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class BoardController {
 
+    private final HttpSession session;
+    private final BoardRepository boardRepository;
+
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        List<Board> boardList = boardRepository.findAll();
+        request.setAttribute("boardList",boardList);
         return "index";
     }
 
