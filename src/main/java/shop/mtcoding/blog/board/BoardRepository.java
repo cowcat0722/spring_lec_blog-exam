2c2bb50cf44cf3eeb4562d59ad4a04d7932fa63b
Pakchan <1625787@donga.ac.kr>
2024-02-08 10:25:14 +0900

    더미데이터 입력

---
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -0,0 +1,12 @@
+package shop.mtcoding.blog.board;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class BoardRepository {
+    private final EntityManager em;
+
+}
